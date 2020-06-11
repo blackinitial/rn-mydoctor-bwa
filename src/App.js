81b67c2bec14a88043bd96d632a323d6faf7e289
blackinitial <alhanif.blackinitial@gmail.com>
2020-06-11 14:05:12 +0700

    setup routing, stack navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 import {Splash, GetStarted} from './pages';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <GetStarted />
+      <Router />
     </NavigationContainer>
   );
 };
