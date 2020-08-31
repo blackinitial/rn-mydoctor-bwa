82303779bc87438834fbc9d006f70e9a543902a9
blackinitial <alhanif.blackinitial@gmail.com>
2020-08-31 20:59:29 +0700

    add botom-tabs lib, add bottom tabs

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,7 +4,7 @@ import {ILLogo} from '../../assets';
 import {Input, Link, Button, Gap} from '../../components';
 import {colors, fonts} from '../../utils';
 
-const Login = () => {
+const Login = ({navigation}) => {
   return (
     <View style={styles.page}>
       <ILLogo />
@@ -17,7 +17,7 @@ const Login = () => {
       <Link title="Forgot My Password" size={12} />
       <Gap height={40} />
 
-      <Button title="Sign In" />
+      <Button title="Sign In" onPress={() => navigation.replace('MainApp')} />
       <Gap height={30} />
       <Link title="Create New Account" size={16} align="center" />
     </View>
