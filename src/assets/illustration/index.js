9326d11899d072caa55d54bf042d4ec43d70581d
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-01 19:34:07 +0700

    add icons, style home profile

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,15 @@
 import ILLogo from './logo.svg';
 import ILGetStarted from './get-started.png';
 import ILNullPhoto from './null-photo.png';
+import ILCatUmum from './cat-doc-umum.svg';
+import ILCatPsikiater from './cat-doc-psikiater.svg';
+import ILCatObat from './cat-doc-obat.svg';
 
-export {ILLogo, ILGetStarted, ILNullPhoto};
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatUmum,
+  ILCatPsikiater,
+  ILCatObat,
+};
