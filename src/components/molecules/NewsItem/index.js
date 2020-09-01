4f36daf6d263e970acf47263815bfe11a7ff1a5b
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-01 18:37:47 +0700

    base comp on doctor page

---
+++ src/components/molecules/NewsItem/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const NewsItem = () => {
+  return (
+    <View>
+      <Text>NewsItem comp</Text>
+    </View>
+  );
+};
+
+export default NewsItem;
+
+const styles = StyleSheet.create({});
---
+++ src/components/molecules/RatedDoctor/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const RatedDoctor = () => {
+  return (
+    <View>
+      <Text>RatedDoctor comp</Text>
+    </View>
+  );
+};
+
+export default RatedDoctor;
+
+const styles = StyleSheet.create({});
