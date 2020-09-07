db91c92b1d578b824cfc77a7188cc1762805f73d
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-07 21:13:28 +0700

    style home doctor category

--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -1,5 +1,6 @@
 const mainColors = {
   green1: '#0BCAD4',
+  green2: '#EDFCFD',
   dark1: '#112340',
   dark2: '#495A75',
   gray1: '#7D8797',
@@ -30,4 +31,5 @@ export const colors = {
     },
   },
   border: mainColors.gray2,
+  cardLight: mainColors.green2,
 };
