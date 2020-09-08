5fdf2e206ed332da0cb2207180f896cc4dc73207
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-08 14:37:19 +0700

    style messages page

---
+++ src/components/molecules/ListDoctor/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {DummyDoctor1} from '../../../assets';
+import {colors, fonts} from '../../../utils';
+
+const ListDoctor = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={DummyDoctor1} style={styles.avatar} />
+      <View>
+        <Text style={styles.name}>Alexander Jannie</Text>
+        <Text style={styles.desc}>
+          Baik ibu, terima kasih banyak atas wakt...
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ListDoctor;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+  },
+  avatar: {width: 46, height: 46, borderRadius: 46 / 2, marginRight: 12},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.primary,
+  },
+  desc: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.secondary,
+  },
+});
