7a9494eb68a641be86de4036e882d30bf7b735c0
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-10 13:34:52 +0700

    slice chat Item

--- src/components/molecules/ChatItem/index.js
+++ src/components/molecules/ChatItem/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import IsMe from './IsMe';
+import Other from './Other';
 
-const ChatItem = () => {
-  return (
-    <View>
-      <Text>ChatItem comp</Text>
-    </View>
-  );
+const ChatItem = ({isMe}) => {
+  if (isMe) {
+    return <IsMe />;
+  }
+  return <Other />;
 };
 
 export default ChatItem;
-
-const styles = StyleSheet.create({});
