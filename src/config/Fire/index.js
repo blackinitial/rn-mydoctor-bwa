fb05ba67d1995cd9e851e944cf3032b266aae9d9
blackinitial <alhanif.blackinitial@gmail.com>
2020-09-29 13:27:46 +0700

    add lib firebase, setup firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAs5AXJ6MbTe-4mK8fzM4F35wKkfCoM-vw',
+  authDomain: 'my-doctor-4a8f9.firebaseapp.com',
+  databaseURL: 'https://my-doctor-4a8f9.firebaseio.com',
+  projectId: 'my-doctor-4a8f9',
+  storageBucket: 'my-doctor-4a8f9.appspot.com',
+  messagingSenderId: '802499017755',
+  appId: '1:802499017755:web:b6ffe02ef956f5451c4cbe',
+});
+
+const Fire = firebase;
+
+export default Fire;
